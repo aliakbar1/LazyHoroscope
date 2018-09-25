a686cefe5b62dcbf13e83963dc482451157cd0da
aliakbar1 <aakbar2@mail.csuchico.edu>
2018-09-24 17:24:16 -0700

    finished code debugged2

--- main.cpp
+++ main.cpp
@@ -11,34 +11,34 @@ string first;
 string last;
 int Lucky;
   //get user input
-cout<<" what is your first name ";
+cout<<"what is your first name?\n";
 cin>> first;
-cout<<" what is your last name ";
+cout<<"what is your last name?\n";
 cin>> last;
 int index=last.length()-1;
   //tell fortune
-cout<< " Welcome, ";
+cout<< "Welcome,";
 cout<<first[0];
 cout<<".";
 cout<<last[0];
-cout<< " Here is your fortune ";
-cout<<" your lucky number is ";
+cout<< ", here is your fortune...\n";
+cout<<"your lucky number is ";
 cout<<first.length()<<endl;
 
 if(first[0]=='a'||first[0]=='i'||first[0]=='e'||first[0]=='o'||first[0]=='u'||first[0]=='A'||first[0]=='E'||first[0]=='I'||first[0]=='O'||first[0]=='U')
 {
-cout<<" you are destined to be famous ";
+cout<<"you are destined to be famous!\n";
 }        
 else
 {
-cout<<" you should keep a low profile ";
+cout<<"you should keep a low profile.\n";
 }
 if(last[index]=='a'||last[index]=='i'||last[index]=='e'||last[index]=='o'||last[index]=='u'||last[index]=='A'||last[index]=='E'||last[index]=='I'||last[index]=='O'||last[index]=='U')
 {
-        cout<<" you have met your true love ";}
+        cout<<"you have met your true love.\n";}
 
 
-        cout<<" have a good day ";
+        cout<<"have a good day!\n";
 
  return 0;
 }
